e272d842e92ffa36d096bd6bcc40500e5a98c9c6
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2023-10-29 17:15:30 -0300

    Adicionei responsividade ao site todo

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// Get the modal
+// Variável do modal
 var card_modal = document.getElementById("modal");
 
-// Get the button that opens the modal
+// Botão que ativa o modal quando clicado
 var botao_modal_1 = document.getElementById("botao_modal_1");
 var botao_modal_2 = document.getElementById("botao_modal_2");
 var botao_modal_3 = document.getElementById("botao_modal_3");
 
-// Get the <span> element that closes the modal
+// Botão de voltar que desativa o modal
 var voltar = document.getElementsByClassName("modal_baixo_botao_voltar")[0];
 
 var sim = document.getElementsByClassName("modal_baixo_botao_sim")[0];
 
-// When the user clicks the button, open the modal 
+// Quando clicar no botão, ativar o mo dal
 botao_modal_1.onclick = function() {
     card_modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
+// Quando clicar em voltar (ou no sim), desativar o modal
 voltar.onclick = function() {
     card_modal.style.display = "none";
 };
@@ -25,9 +25,28 @@ sim.onclick = function() {
     card_modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// Desativar o modal se clicar fora dele
 window.onclick = function(event) {
   if (event.target == modal) {
     card_modal.style.display = "none";
   };
 };
+
+
+/* Funcionalidade Menu Hamburguer */
+
+// Pegar o menu de hamburguer e as opções do menu
+hamburguer = document.querySelector(".navbar_hamburguer");
+menu = document.querySelector(".navbar_menu");
+
+// Quando clicar no menu de hamburguer, ganhará uma classe chamada "showing", 
+// e enquanto o menu de hamburguer ter essa classe as opções aparecerão
+// Se clicar denovo (para fechar), fecha as opções do menu e tira a classe "showing" do menu de hamburguer.
+hamburguer.onclick = function() {
+  menu.style.display = 'flex';
+  menu.classList.toggle("showing");
+  if (menu.classList.contains("showing")) {
+    menu.style.display = 'none';
+  }
+
+}
