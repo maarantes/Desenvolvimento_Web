f49790976ab841388a54cbb328f08e1cf4058fba
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2023-11-10 14:20:53 -0300

    Adicionei o Flask no site todo

--- static/script.js
+++ static/script.js
@@ -35,18 +35,32 @@ window.onclick = function(event) {
 
 /* Funcionalidade Menu Hamburguer */
 
-// Pegar o menu de hamburguer e as opções do menu
 hamburguer = document.querySelector(".navbar_hamburguer");
 menu = document.querySelector(".navbar_menu");
 
-// Quando clicar no menu de hamburguer, ganhará uma classe chamada "showing", 
-// e enquanto o menu de hamburguer ter essa classe as opções aparecerão
-// Se clicar denovo (para fechar), fecha as opções do menu e tira a classe "showing" do menu de hamburguer.
+//Se apertar o menu ganha a classe showing, se apertar denovo remove a classe showing
 hamburguer.onclick = function() {
-  menu.style.display = 'flex';
+  menu.style.display = "flex";
   menu.classList.toggle("showing");
-  if (menu.classList.contains("showing")) {
-    menu.style.display = 'none';
-  }
 
+}
+
+
+/* Deixar página ativa em azul no Navbar */
+
+var activePage = window.location.pathname;
+let nav_home = document.getElementById("nav_home");
+let nav_sobre = document.getElementById("nav_sobre");
+let nav_contato = document.getElementById("nav_contato");
+
+if (activePage == "/") {
+  nav_home.classList.toggle("active");
+}
+
+if (activePage == "/sobre") {
+  nav_sobre.classList.toggle("active");
+}
+
+if (activePage == "/contato") {
+  nav_contato.classList.toggle("active");
 }
